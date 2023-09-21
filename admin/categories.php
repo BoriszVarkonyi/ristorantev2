b39dee5e9c4bbbed8fc707a5602ae4972da87850
Borisz Varkonyi <borisz.varkonyi@gmail.com>
2023-09-21 18:38:30 +0200

    cat_base

--- admin/categories.php
+++ admin/categories.php
@@ -1,1 +1,25 @@
 <?php
+global $connection;
+include '../db.php';
+
+$sql = "SELECT * FROM categories";
+
+$result = mysqli_query($connection, $sql);
+
+?>
+
+<html>
+<h1>MENU CATEGORIES</h1>
+<ul>
+    <?php
+
+    while ($row = mysqli_fetch_array($result)){
+
+    ?>
+    <li><?php echo $row["cat_name"]?></li>
+    <?php
+    }
+    ?>
+</ul>
+
+</html>
--- admin/functions.php
+++ admin/functions.php
@@ -1,5 +1,5 @@
 <html>
-<a href="galery.php">Galery</a>
+<a href="gallery.php">Gallery</a>
 <a href="menu.php">Menu</a>
 <a href="categories.php">Categories</a>
 </html>
